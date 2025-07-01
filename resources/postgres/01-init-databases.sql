e6dcec42668117ff918ad8c5674d8a30f2c577b1
Jaehyeon Kim <dottami@gmail.com>
2025-07-01 16:59:23 +1000

    simplify docker compose configuration

--- resources/postgres/01-init-databases.sql
+++ resources/postgres/01-init-databases.sql
@@ -1,9 +1,6 @@
--- Create database
-CREATE DATABASE factorhouse;
-
--- Switch context to the new DB (note: this only works when using psql)
-\connect factorhouse;
-
+-- //
+-- // Configure the main development database
+-- //
 -- Create schema
 CREATE SCHEMA IF NOT EXISTS demo;
 
@@ -11,10 +8,19 @@ CREATE SCHEMA IF NOT EXISTS demo;
 GRANT ALL ON SCHEMA demo TO db_user;
 
 -- Set search_path at the DB level
-ALTER DATABASE factorhouse SET search_path TO demo, public;
+ALTER DATABASE fh_dev SET search_path TO demo, public;
 
 -- Set search_path for current session too
 SET search_path TO demo, public;
 
 -- Create CDC publication for Debezium
 CREATE PUBLICATION cdc_pub FOR TABLES IN SCHEMA demo;
+
+-- //
+-- // Create the 'metastore' database for the Hive metastore
+-- //
+CREATE DATABASE metastore;
+
+\connect metastore;
+
+GRANT ALL PRIVILEGES ON DATABASE metastore TO db_user;
