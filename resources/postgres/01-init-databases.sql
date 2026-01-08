b41f9a9e79814fd05e22d4691e66ff150995b12b
Jaehyeon Kim <dottami@gmail.com>
2026-01-08 13:47:10 +1100

    enable pg_stat_statements

--- resources/postgres/01-init-databases.sql
+++ resources/postgres/01-init-databases.sql
@@ -16,6 +16,11 @@ SET search_path TO demo, public;
 -- Create CDC publication for Debezium
 CREATE PUBLICATION cdc_pub FOR TABLES IN SCHEMA demo;
 
+-- Create pg_stat_statements extension for fh_dev
+CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+-- SHOW shared_preload_libraries;
+-- SELECT COUNT(*) FROM pg_stat_statements;
+
 -- //
 -- // Create the 'metastore' database for the Hive metastore
 -- //
@@ -24,3 +29,9 @@ CREATE DATABASE metastore;
 \connect metastore;
 
 GRANT ALL PRIVILEGES ON DATABASE metastore TO db_user;
+
+-- Create pg_stat_statements extension for metastore
+CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+
+-- Grant read access to stats for db_user
+GRANT pg_read_all_stats TO db_user;
